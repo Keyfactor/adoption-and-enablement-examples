2a640a023cf87053d9ba4e02c6320806335f76c9
Jeremy Howland <Jeremy.Howland@Keyfactor.com>
2025-09-18 22:52:33 -0400

    fixed pagination

--- RoleNameUpdate/RoleNameUpdate.ps1
+++ RoleNameUpdate/RoleNameUpdate.ps1
@@ -93,12 +93,12 @@ function load_variables
         'Lab'
         {
             $script:envVariables = @{
-                CLIENT_ID       = ''
-                CLIENT_SECRET   = ''
-                TOKEN_URL       = ''
-                SCOPE           = ''
-                AUDIENCE        = ''
-                KEYFACTORAPI    = ''
+                CLIENT_ID       = 'd424706f-3c5f-453e-8b6e-2be54788bd17'
+                CLIENT_SECRET   = 'Z5ETEC_mY8Z6McI8t2KRz5h-eXeQYm~1y0xngF06VlF8.ttCtDX1vAAjAO_U6ghm'
+                TOKEN_URL       = 'https://auth.pingone.com/3729a543-20bf-44b1-b92b-7ceef13aeecf/as/token'
+                SCOPE           = 'APISCOPE'
+                AUDIENCE        = 'APISCOPE'
+                KEYFACTORAPI    = 'https://boeingoauth.kfdelivery.com/KeyfactorAPI'
             }
         }
     }
@@ -249,7 +249,7 @@ function Fetch_AllPages
     write-message -Message "Entering function Fetch_AllPages" -type Debug
     $TotalResults = @()
     $InitialResponse = Invoke-WebRequest "$Url" -Method Get -Headers (get-AuthHeaders -HeaderVersion $HeaderVersion) -UseBasicParsing
-    $TotalCount = $InitialResponse.Headers["x-total-count"]
+    $TotalCount = $InitialResponse.Headers["x-total-count"][0]
     write-message -Message "Total count of items to fetch: $TotalCount" -type Verbose
     if ($TotalCount -lt 50) {
         write-message -Message "Total count is less than page size limit. Returning initial response." -type Verbose
@@ -264,7 +264,9 @@ function Fetch_AllPages
         write-message -Message "Fetching page $CurrentPage/$TotalPages from URL=$PageUrl" -type Verbose
         $FullUrl = "$PageUrl$CurrentPage"
         $Response = Invoke-WebRequest $FullUrl -Headers (get-AuthHeaders -HeaderVersion $HeaderVersion) -Method Get
-        $TotalResults += $Response.Content | ConvertFrom-Json
+        # $Response = $Response | ConvertFrom-Json
+        write-host "$($Response.count)"
+        $TotalResults += $Response
     }
     write-message -Message "Finished fetching all pages. Total results: $($TotalResults.Count)" -type Verbose
     return $TotalResults
